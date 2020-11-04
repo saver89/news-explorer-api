0301053d10e08390a4d713e613bbc02bcba6009e
Vladimir <neelkim1989@gmail.com>
2020-11-04 10:38:07 +0500

    Добавить схемы article и user

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
+require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: [
+    'https://nee.students.nomoreparties.co',
+    'https://www.nee.students.nomoreparties.co',
+  ],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
+});
 
 app.listen(PORT);
